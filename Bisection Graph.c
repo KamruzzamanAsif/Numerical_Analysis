f26bf25bf02272f05a5fa3a0697899993c7631aa
Kamruzzaman Asif <72350695+KamruzzamanAsif@users.noreply.github.com>
2022-03-03 01:15:15 -0800

    Add files via upload

---
+++ Bisection Graph.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+#define ep 10e-9
+
+double function(double x){
+    return (x*x*x)+(4*(x*x))-10;
+}
+
+double a = 1;
+double b=2;
+double c;
+
+int main(void){
+    if(function(a)*function(b)>0){
+        printf("No root");
+        return -1;
+    }
+
+    printf("Itr,Root");
+    int itr = 0;
+    while(1){
+        itr++;
+
+        c = (a+b)/2;
+
+        double value = function(c);
+        printf("\n%d,%lf",itr,fabs(value));
+        if( fabs(value) <= ep ){
+            break;
+        }
+        // next itr
+        if(function(a)*function(c) < 0.0)
+            b = c;
+        else
+            a = c;
+    }
+
+   // printf("Root is: %.6lf", c);
+   // printf("\nTotal itr = %d",itr);
+
+    return 0;
+}
---
+++ False position.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+#define ep 10e-9
+
+double function(double x){
+    return (x*x*x)+(4*(x*x))-10;
+}
+
+double a = 1;
+double b=2;
+double c;
+
+int main(void){
+    if(function(a)*function(b)>0){
+        printf("No root");
+        return -1;
+    }
+
+    int itr = 0;
+    while(1){
+        itr++;
+
+        c = ((a*function(b))-(b*function(a))) / (function(b)-function(a));
+
+        double value = function(c);
+        if( fabs(value) <= ep ){
+            break;
+        }
+        // next itr
+        if(function(a)*function(c) < 0.0)
+            b = c;
+        else
+            a = c;
+    }
+
+    printf("Root is: %.9lf", c);
+    printf("\nTotal itr = %d",itr);
+
+    return 0;
+}
